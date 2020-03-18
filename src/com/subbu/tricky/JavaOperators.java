09e05c8919bcc317a376bece10d24a5aed195ac9
Subbarao Paritala <subbarao.paritala@cesltd.com>
2020-03-18 17:14:42 +0530

    Started working on Operators

---
+++ src/com/subbu/tricky/JavaOperators.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.subbu.tricky;
+
+/**
+ * @author subbaraop
+ *
+ */
+public class JavaOperators {
+	public static void main(String[] args) {
+
+	}
+
+	static void assignment() {
+		int a = 10;
+		String name = "Subbarao";
+//		System.out.println("Name of the Employee is :" + name + "Emp id is :" + a);
+		System.out.println(a);
+		
+	}
+}
